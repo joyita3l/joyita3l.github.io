223a438e976b05885e69e01ef3fe41a5d7bdd008
joyita3l <108160663+joyita3l@users.noreply.github.com>
2022-06-26 12:45:35 -0500

    Added new files

---
+++ script.js
@@ -0,0 +1,137 @@
+const counters = document.querySelectorAll('.counter');
+const speed = 500; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = target / speed;
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = Math.ceil(count + inc) ;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
+
+
+// Testimonials carousel (uses the Owl Carousel library)
+// Testimonials carousel (uses the Owl Carousel library)
+  $(".testimoniales-carousel").owlCarousel({
+    items: 1,
+	autoplay: true,
+	smartSpeed: 100,
+    loop: true,
+	autoPlayHoverPause: true
+});
+
+//SLIDES GUIA METODOLOGICA-----------------------------------------------------------------------------------------
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+//SLIDES PEKESTAR1-----------------------------------------------------------------------------------------
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n2) {
+  showSlides2(slideIndex2 += n2);
+}
+
+// Thumbnail image controls
+function currentSlide2(n2) {
+  showSlides2(slideIndex2 = n2);
+}
+
+function showSlides2(n2) {
+  let i2;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  let dots2 = document.getElementsByClassName("demo2");
+  let captionText2 = document.getElementById("caption2");
+  if (n2 > slides2.length) {slideIndex2 = 1}
+  if (n2 < 1) {slideIndex2 = slides2.length}
+  for (i2 = 0; i2 < slides2.length; i2++) {
+    slides2[i2].style.display = "none";
+  }
+  for (i2 = 0; i2 < dots2.length; i2++) {
+    dots2[i2].className = dots2[i2].className.replace(" active2", "");
+  }
+  slides2[slideIndex2-1].style.display = "block";
+  dots2[slideIndex2-1].className += " active2";
+  captionText2.innerHTML = dots2[slideIndex2-1].alt;
+}
+
+
+//SLIDES JOYITA CERO-----------------------------------------------------------------------------------------
+
+let slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+// Next/previous controls
+function plusSlides3(n3) {
+  showSlides3(slideIndex3 += n3);
+}
+
+// Thumbnail image controls
+function currentSlide3(n3) {
+  showSlides3(slideIndex3 = n3);
+}
+
+function showSlides3(n3) {
+  let i3;
+  let slides3 = document.getElementsByClassName("mySlides3");
+  let dots3 = document.getElementsByClassName("demo3");
+  let captionText3 = document.getElementById("caption3");
+  if (n3 > slides3.length) {slideIndex3 = 1}
+  if (n3 < 1) {slideIndex3 = slides3.length}
+  for (i3 = 0; i3 < slides3.length; i3++) {
+    slides3[i3].style.display = "none";
+  }
+  for (i3 = 0; i3 < dots3.length; i3++) {
+    dots3[i3].className = dots3[i3].className.replace(" active3", "");
+  }
+  slides3[slideIndex3-1].style.display = "block";
+  dots3[slideIndex3-1].className += " active3";
+  captionText3.innerHTML = dots3[slideIndex3-1].alt;
+}
+
+
